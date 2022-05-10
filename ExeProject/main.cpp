64c5cfe019316a930451386982c10c4947a17c4e
DxBillGates <suzu.tomo283@gmail.com>
2022-05-10 12:13:20 +0900

    初期解像度変更

--- ExeProject/main.cpp
+++ ExeProject/main.cpp
@@ -11,7 +11,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 #endif
 
 	GE::Math::Vector2 windowSize = GE::Math::Vector2(1280, 720);
-	GE::Math::Vector2 resolution = GE::Math::Vector2(1920, 1080);
+	GE::Math::Vector2 resolution = GE::Math::Vector2(1280, 720);
 
 	Game game = Game(windowSize, resolution, "no title", GE::WindowMode::WINDOW);
 
